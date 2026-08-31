eae9de84c2124d22c32685af1f3b0930a1672ef7
Rafael Filastro <rafael.filastro@gmail.com>
2026-08-31 16:13:06 -0300

    chore: cria estrutura inicial e configura banco de dados

---
+++ src/settings/settings.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+RAIZ_PROJETO = Path(__file__).resolve().parents[2]
+
+load_dotenv(RAIZ_PROJETO / ".env")
+
+class Settings:
+    def __init__(self):
+        self.db_host: str = os.getenv("DB_HOST")
+        self.db_port: int = int(os.getenv("DB_PORT"))
+        self.db_user: str = os.getenv("DB_USER")
+        self.db_password: str = os.getenv("DB_PASS")
+        self.db_name: str = os.getenv("DB_NAME")
+
+        self.app_host: str = os.getenv("APP_HOST")
+        self.app_port: int = int(os.getenv("APP_PORT"))
+
+
+configuracoes = Settings()
